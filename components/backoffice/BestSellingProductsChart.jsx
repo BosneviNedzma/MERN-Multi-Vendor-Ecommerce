21bda977589ca7ab25a25316f112111254ade19e
Nedžma Brkić <brkic.nedzma.21@size.ba>
2024-03-27 23:05:51 +0100

    Update BestSellingProductsChart.jsx

--- components/backoffice/BestSellingProductsChart.jsx
+++ components/backoffice/BestSellingProductsChart.jsx
@@ -28,7 +28,7 @@ export default function BestSellingProductsChart() {
     ],
   };
 
-Hello World
+Hello Worldfgg
   return (
     <div className="bg-slate-700 p-8 rounded-lg">
       <h2 className="text-xl font-bold mb-4">Best Selling Charts</h2>
