b32b626528e9e13d1c45a81dd8b7de5287c28dbb
Nedzma Brkic <brkicnedzma@gmail.com>
2024-03-29 23:53:52 +0100

    Dark mode

--- components/backoffice/Navbar.jsx
+++ components/backoffice/Navbar.jsx
@@ -1,26 +1,35 @@
 import { AlignJustify, Bell, Sun, User } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export default function Navbar() {
   return (
     <div
-      className="flex items-center justify-between bg-slate-800 text-slate-50 h-16 px-8 py-4
-    fixed top-0 w-full left-60"
+      className="flex items-center justify-between bg-slate-800 text-slate-50 h-20 px-8 py-8
+    fixed top-0 w-full left-60 z-50 pr-[20rem]"
     >
       {/* Icon */}
       <button>
         <AlignJustify />
       </button>
       {/* 3 Icons */}
-      <div className="flex space-x-3">
+      <div className="flex space-x-3 text-green-600">
         <button>
           <Sun />
         </button>
-        <button>
+
+        <button
+          type="button"
+          className="relative inline-flex items-center p-3 text-sm font-medium text-center bg-transparent rounded-lg"
+        >
           <Bell />
+          <span className="sr-only">Notifications</span>
+          <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
+            20
+          </div>
         </button>
         <button>
-          <User />
+          <Image src='/profile.jpg' alt="User profile" width={200} height={200} className="w-8 h-8 rounded-full"  />
         </button>
       </div>
     </div>
