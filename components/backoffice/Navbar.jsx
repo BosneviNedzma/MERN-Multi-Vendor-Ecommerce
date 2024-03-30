d662afca87e9fb1e5d04335a4d9d5e0dedf22f31
Nedzma Brkic <brkicnedzma@gmail.com>
2024-03-30 23:55:45 +0100

    Update Dark Mode

--- components/backoffice/Navbar.jsx
+++ components/backoffice/Navbar.jsx
@@ -1,4 +1,20 @@
-import { AlignJustify, Bell, Sun, User } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlignJustify,
+  Bell,
+  LayoutDashboard,
+  LogOut,
+  Sun,
+  UserCog,
+  X,
+} from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -18,19 +34,88 @@ export default function Navbar() {
           <Sun />
         </button>
 
-        <button
-          type="button"
-          className="relative inline-flex items-center p-3 text-sm font-medium text-center bg-transparent rounded-lg"
-        >
-          <Bell />
-          <span className="sr-only">Notifications</span>
-          <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
-            20
-          </div>
-        </button>
-        <button>
-          <Image src='/profile.jpg' alt="User profile" width={200} height={200} className="w-8 h-8 rounded-full"  />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <button
+              type="button"
+              className="relative inline-flex items-center p-3 text-sm font-medium text-center bg-transparent rounded-lg"
+            >
+              <Bell />
+              <span className="sr-only">Notifications</span>
+              <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
+                20
+              </div>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="py-2 px-4 pr-8">
+            <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem>
+              <div className="flex items-center">
+                <Image
+                  src="/profile.jpg"
+                  alt="User profile"
+                  width={200}
+                  height={200}
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col">
+                  <p>Yellow Sweet Corn Stock out</p>
+                  <div className="flex">
+                    <p className="px-2 py-1 bg-red-700 text-white rounded-full">Stock Out</p>
+                    <p>Dec 12 2021 - 12.40</p>
+                  </div>
+                </div>
+                <button>
+                  <X />
+                </button>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <button className="flex items-center">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <button>
+              <Image
+                src="/profile.jpg"
+                alt="User profile"
+                width={200}
+                height={200}
+                className="w-8 h-8 rounded-full"
+              />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="py-2 px-4 pr-8">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <button className="flex items-center">
+                <LayoutDashboard className="mr-2 h-4 w-4" />
+                <span>Dashboard</span>
+              </button>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <button className="flex items-center">
+                <UserCog className="mr-2 h-4 w-4" />
+                <span>Edit Profile</span>
+              </button>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <button className="flex items-center">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
